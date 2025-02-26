a8040edce766fda62a22baabf9d92aef2bd154e2
smallmushroomovo <jxk370@126.com>
2025-02-26 19:27:51 +0800

    Use Mirror

--- update.bat
+++ update.bat
@@ -1,10 +1,10 @@
 @echo off
-echo 下载依赖文件中...
+echo ?????????????...
  if  not exist ".\packwiz-installer-bootstrap.jar" (
-  powershell -Command Invoke-WebRequest -Uri "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar" -OutFile ".\packwiz-installer-bootstrap.jar"
+  powershell -Command Invoke-WebRequest -Uri "https://ghfast.top/github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar" -OutFile ".\packwiz-installer-bootstrap.jar"
 )
 if not exist ".\packwiz-installer.jar" (
-  powershell -Command Invoke-WebRequest -Uri "https://github.com/packwiz/packwiz-installer/releases/download/v0.5.14/packwiz-installer.jar" -OutFile ".\packwiz-installer.jar"
+  powershell -Command Invoke-WebRequest -Uri "https://ghfast.top/github.com/packwiz/packwiz-installer/releases/download/v0.5.14/packwiz-installer.jar" -OutFile ".\packwiz-installer.jar"
 )
-echo 正在尝试更新...
+echo ??????????...
 java -jar packwiz-installer-bootstrap.jar {URL}/pack.toml
