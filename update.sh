df26f811390787a2b978e4d8be2d00dffe1086fa
smallmushroomovo <jxk370@126.com>
2025-02-27 20:18:19 +0800

    Use Mirror

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ echo "下载依赖文件中..."
 if [ ! -f "$BOOTSTRAP_JAR" ]; then
     echo "正在下载 $BOOTSTRAP_JAR..."
     if ! curl -fL -o "$BOOTSTRAP_JAR" \
-        "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/$BOOTSTRAP_JAR"; 
+        "https://ghfast.top/github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/$BOOTSTRAP_JAR"; 
     then
         echo "下载失败，请检查网络连接或证书配置"
         exit 2
@@ -22,7 +22,7 @@ fi
 if [ ! -f "$INSTALLER_JAR" ]; then
     echo "正在下载 $INSTALLER_JAR..."
     if ! curl -fL -o "$INSTALLER_JAR" \
-        "https://github.com/packwiz/packwiz-installer/releases/download/v0.5.14/$INSTALLER_JAR";
+        "https://ghfast.top/github.com/packwiz/packwiz-installer/releases/download/v0.5.14/$INSTALLER_JAR";
     then
         echo "下载失败，请检查网络连接或证书配置"
         exit 3
